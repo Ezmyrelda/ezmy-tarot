338c0e43ccb59838e5e97746912f7b1c0a14e3cf
Ezmyrelda <dj.ezmyrelda@gmail.com>
2015-02-13 13:21:22 -0800

    Fixes?

--- tarot.rb
+++ tarot.rb
@@ -1,8 +1,4 @@
 #!/usr/bin/ruby
-print "You would like your fortune? Please tell me your name; or should I read that as well?"
-name = gets.chomp
-print "How many cards would you like?"
-reading = gets.chomp
 
 print "You would like your fortune? Please tell me your name; or should I read that as well?"
 name = gets.chomp
